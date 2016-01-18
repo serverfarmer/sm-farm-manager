b59ae386226bfcd13fb84e9e6c85c8bae584c18b
Tomasz Klim <github@tomaszklim.pl>
2016-01-18 19:51:16 +0100

    support for ssh on non-standard port

--- add-managed-host.sh
+++ add-managed-host.sh
@@ -4,45 +4,56 @@
 path="/etc/local/.config"
 
 if [ "$1" = "" ]; then
-	echo "usage: $0 <hostname>"
+	echo "usage: $0 <hostname[:port]>"
 	exit 1
-elif ! [[ $1 =~ ^[a-z0-9.-]+[.][a-z0-9]+$ ]]; then
+elif ! [[ $1 =~ ^[a-z0-9.-]+[.][a-z0-9]+([:][0-9]+)?$ ]]; then
 	echo "error: parameter $1 not conforming hostname format"
-	exit 1
-elif [ "`getent hosts $1`" = "" ]; then
-	echo "error: host $1 not found"
-	exit 1
-elif [ "`cat $path/*.hosts |grep \"^$1$\"`" != "" ]; then
-	echo "error: host $1 already added"
 	exit 1
 fi
 
-sshkey=`ssh_management_key_storage_filename $1`
-ssh -i $sshkey -o StrictHostKeyChecking=no -o PasswordAuthentication=no root@$1 uptime >/dev/null 2>/dev/null
+server=$1
+if [ -z "${server##*:*}" ]; then
+	host="${server%:*}"
+	port="${server##*:}"
+else
+	host=$server
+	port=22
+fi
+
+if [ "`getent hosts $host`" = "" ]; then
+	echo "error: host $host not found"
+	exit 1
+elif [ "`cat $path/*.hosts |grep \"^$host$\"`" != "" ]; then
+	echo "error: host $host already added"
+	exit 1
+fi
+
+sshkey=`ssh_management_key_storage_filename $host`
+ssh -i $sshkey -p $port -o StrictHostKeyChecking=no -o PasswordAuthentication=no root@$host uptime >/dev/null 2>/dev/null
 
 if [[ $? != 0 ]]; then
-	echo "error: host $1 denied access"
+	echo "error: host $server denied access"
 	exit 1
 fi
 
 if [ -x /opt/sf-backup-collector/add-backup-host.sh ]; then
-	/opt/sf-backup-collector/add-backup-host.sh $1
+	/opt/sf-backup-collector/add-backup-host.sh $server
 fi
 
-hwtype=`ssh -i $sshkey root@$1 /opt/farm/scripts/config/detect-hardware-type.sh`
-openvz=`ssh -i $sshkey root@$1 "cat /proc/vz/version 2>/dev/null"`
-netmgr=`ssh -i $sshkey root@$1 "ls /etc/NetworkManager 2>/dev/null"`
+hwtype=`ssh -i $sshkey -p $port root@$host /opt/farm/scripts/config/detect-hardware-type.sh`
+openvz=`ssh -i $sshkey -p $port root@$host "cat /proc/vz/version 2>/dev/null"`
+netmgr=`ssh -i $sshkey -p $port root@$host "ls /etc/NetworkManager 2>/dev/null"`
 
 if [ "$netmgr" != "" ]; then
-	echo $1 >>"$path/workstation.hosts"
+	echo $server >>"$path/workstation.hosts"
 elif [ $hwtype = "physical" ]; then
-	echo $1 >>"$path/physical.hosts"
+	echo $server >>"$path/physical.hosts"
 elif [ $hwtype = "guest" ]; then
-	echo $1 >>"$path/virtual.hosts"
+	echo $server >>"$path/virtual.hosts"
 fi
 
 if [ "$openvz" != "" ]; then
-	echo $1 >>"$path/openvz.hosts"
+	echo $server >>"$path/openvz.hosts"
 fi
 
 # TODO: implement checking, if added host runs also Xen / LXC / Docker containers
