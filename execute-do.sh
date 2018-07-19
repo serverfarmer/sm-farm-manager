ca626e2f6b6d35870903bd561978de3d65874e8b
Tomasz Klim <github@tomaszklim.pl>
2018-07-19 21:37:08 +0200

    more messages

--- execute-do.sh
+++ execute-do.sh
@@ -20,12 +20,14 @@ exec_openvz() {
 	sshkey=$3
 	command=$4
 
+	echo
+	echo "####### fetching OpenVZ containers list from server $host"
 	containers="`ssh -i $sshkey -p $port root@$host \"/usr/sbin/vzlist -Ho ctid\"`"
 
 	for ID in $containers; do
 		cthost="`ssh -i $sshkey -p $port root@$host \"/usr/sbin/vzlist -Ho hostname $ID\"`"
 		echo
-		echo "####### executing \"$command\" on container $ID [$cthost] at server $hostr"
+		echo "####### executing \"$command\" on container $ID [$cthost] at server $host"
 		ssh -t -i $sshkey -p $port root@$host "/usr/sbin/vzctl exec $ID \"TERM=vt100 $command\""
 	done
 }
@@ -36,6 +38,8 @@ exec_docker() {
 	sshkey=$3
 	command=$4
 
+	echo
+	echo "####### fetching Docker containers list from server $host"
 	containers="`ssh -i $sshkey -p $port root@$host \"docker ps -q\"`"
 
 	for ID in $containers; do
